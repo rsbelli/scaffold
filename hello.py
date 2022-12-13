406fa63bc2d1ccb8715d341c0f15ba35d33bc997
Rafael Speranza Belli <rsbelli@gmail.com>
2022-12-13 20:14:53 +0000

    Black changes when running on Azure.

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
 def toyou(x):
     return f"Hello {x}"
 
+
 def add(x):
     return x + 1
+
 
 def subtract(x):
     return x - 1
--- test_hello.py
+++ test_hello.py
@@ -1,18 +1,23 @@
 from hello import toyou, add, subtract
 
+
 def setup_function(function):
     print(f" Running setup: {function.__name__}")
     function.x = 10
+
 
 def teardown_function(function):
     print(f" Running teardown: {function.__name__}")
     del function.x
 
+
 def test_hello_subtract():
     assert subtract(test_hello_subtract.x) == 9
 
+
 def test_hello_add():
     assert add(test_hello_add.x) == 11
+
 
 def test_hello_toyou():
     assert toyou(test_hello_toyou.x) == "Hello 10"
