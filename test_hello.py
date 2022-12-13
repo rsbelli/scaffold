6d373ae5344235324b3d6057d53da964df155b2b
Rafael Speranza Belli <rsbelli@gmail.com>
2022-12-13 18:03:28 +0000

    Adding changes to the functions

--- test_hello.py
+++ test_hello.py
@@ -1,4 +1,15 @@
-from hello import add
+from hello import toyou, add, subtract
 
-def test_add():
-    assert add(1,2) == 3
+def setup_function(function):
+    print(f" Running setup: {function.__name__}")
+    function.x = 10
+
+def teardown_function(function):
+    print(f" Running teardown: {function.__name__}")
+    del function.x
+
+def test_hello_subtract():
+    assert subtract(test_hello_subtract.x) == 9
+
+def test_hello_add():
+    assert add(test_hello_add.x) == 11
